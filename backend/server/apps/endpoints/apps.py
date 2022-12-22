322a456419a4a6b079011dcfb6d8ef3c7d5c1f91
AI Code Tech <aicodetech@AIs-MacBook-Air.local>
2022-12-22 16:38:18 +0530

    add 3

---
+++ backend/server/apps/endpoints/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class EndpointsConfig(AppConfig):
+    name = 'endpoints'
+# class myAppNameConfig(AppConfig):
+#     name = 'myAppName'
+#     verbose_name = 'A Much Better Name'
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'apps.endpoints.apps.EndpointsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.endpoints'
 ]
 
 MIDDLEWARE = [
