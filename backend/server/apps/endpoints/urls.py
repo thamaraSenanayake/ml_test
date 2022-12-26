811c31dd719f8fce01a406b53ff6d11877d20203
AI Code Tech <aicodetech@AIs-MacBook-Air.local>
2022-12-26 15:36:38 +0530

    finish

--- backend/server/apps/endpoints/urls.py
+++ backend/server/apps/endpoints/urls.py
@@ -1,18 +1,29 @@
-from django.conf.urls import include
+from django.conf.urls import  include
 from rest_framework.routers import DefaultRouter
-from django.urls import path
+from django.urls import path , re_path
 
 from apps.endpoints.views import EndpointViewSet
 from apps.endpoints.views import MLAlgorithmViewSet
 from apps.endpoints.views import MLAlgorithmStatusViewSet
 from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.views import PredictView
+from apps.endpoints.views import StopABTestView
+from apps.endpoints.views import ABTestViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
 router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
 router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+router.register(r"abtests", ABTestViewSet, basename="abtests")
+
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+    re_path(
+        r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
+    re_path(
+        r"^api/v1/stop_ab_test/(?P<ab_test_id>.+)", StopABTestView.as_view(), name="stop_ab"
+    ),
 ]
